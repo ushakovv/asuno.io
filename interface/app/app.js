de708d1f157b1fc79b3bfc62575d02fe1949d462
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-07-16 13:20:50 +0300

    Socet connection off

--- interface/app/app.js
+++ interface/app/app.js
@@ -507,7 +507,7 @@
     })
     .constant('asunoSessionCookie', 'asuno-user')
     .constant('tickEvent', 'asuno.tick')
-    .run(function ($rootScope, $interval, $http, $state, $modal, $sci, Auth, ControllersStoreConstants, FilterSvc, ControllersActions, MassOperations, RemoteCommandListener, tickEvent, RemoteCommandActions, RemoteCommandSocket) {
+    .run(function ($rootScope, $interval, $http, $state, $modal, Auth, ControllersStoreConstants, FilterSvc, ControllersActions, MassOperations, RemoteCommandListener, tickEvent) {
       let timeouts = {
         default: 10000,
         more: 3000,
@@ -607,15 +607,15 @@
 
       $rootScope.$on('$destroy', () => $interval.cancel(tick));
 
-      $rootScope.$on('logged-in', () => {
-        $sci.connect();
-        RemoteCommandListener.installListener();
-        RemoteCommandSocket.installListener();
-      });
+      //$rootScope.$on('logged-in', () => {
+      //  $sci.connect();
+      //  RemoteCommandListener.installListener();
+      //  RemoteCommandSocket.installListener();
+      //});
 
       $rootScope.$on('logged-out', () => {
-        RemoteCommandListener.removeListener();
-        RemoteCommandSocket.removeListener();
+        //RemoteCommandListener.removeListener();
+        //RemoteCommandSocket.removeListener();
         $state.go('login');
       });
 
@@ -625,6 +625,6 @@
         $rootScope.$broadcast('logged-in');
       }
 
-      MassOperations.installListener();
+      //MassOperations.installListener();
     });
 })();
