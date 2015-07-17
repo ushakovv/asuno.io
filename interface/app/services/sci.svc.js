c277b3a24f8f9bc939214aa5126dbf9151a6198d
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-07-17 17:39:05 +0300

    Fix socket off

--- interface/app/services/sci.svc.js
+++ interface/app/services/sci.svc.js
@@ -5,11 +5,21 @@
     this.socket = void 0;
 
     this.connect = function () {
-      this.socket = io.connect('http://81.88.217.69/sci', {
-        query: `authorization=${Auth.session().access_token}`,
-        'force new connection': true
-      });
+      //this.socket = io.connect('http://81.88.217.69/sci', {
+      //  query: `authorization=${Auth.session().access_token}`,
+      //  'force new connection': true
+      //});
+      this.createFake();
       return this.socket;
+    };
+
+    this.createFake = function () {
+      this.socket = {
+        on: () => false,
+        emit: () => false,
+        removeListener: () => false,
+        connected: false
+      };
     };
   }
 
